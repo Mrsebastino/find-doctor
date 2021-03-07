fa926d64a37cb90312574ef5647f6521c8d398fc
mrsebastino <mrsebastino@gmail.com>
2021-03-07 17:03:22 +0000

    set up static files and map with node

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
 app.get("/", (req, res) => {
-    res.send("hello");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000);
